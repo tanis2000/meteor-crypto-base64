52ea8cca579a6847e3c1e3f42a6cb04eeb8894b9
Valerio Santinelli <santinelli@altralogica.it>
2014-10-21 09:33:59 +0200

    Moving to the new plugin format

--- package.js
+++ package.js
@@ -1,10 +1,14 @@
 Package.describe({
-  summary: "Base64 (en/de)coding for CryptoJS, standard secure crypto algorithms"
+  summary: "Base64 (en/de)coding for CryptoJS, standard secure crypto algorithms",
+  version: "3.1.2.0-1",
+  git: "https://github.com/tanis2000/meteor-crypto-base64.git"
 });
 
 Package.on_use(function (api, where) {
+	api.versionsFrom('METEOR@0.9.1.1');
   api.use('crypto-base', ['client', 'server']);
   api.use('underscore', ['client', 'server']);
+  api.imply('crypto-base', ['client', 'server']);
   api.add_files('enc-base64.js', ['client', 'server']);
 });
 
